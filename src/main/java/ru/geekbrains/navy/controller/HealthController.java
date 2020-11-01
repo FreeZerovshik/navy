4c63cbd6fcba272fdbddcf5f7dea5f54639872ba
llebidko <llebidko@gmail.com>
2020-11-01 21:32:01 +0300

    webianr initial state

--- src/main/java/ru/geekbrains/navy/controller/HealthController.java
+++ src/main/java/ru/geekbrains/navy/controller/HealthController.java
@@ -1,41 +1,16 @@
 package ru.geekbrains.navy.controller;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
-@Api(value = "/health", tags = {"Проверка состояния сервиса"})
+@Controller
 public class HealthController {
 
-    @GetMapping("/health")
-    @ApiOperation(
-            value = "Полученить состояние сервиса",
-            httpMethod = "GET",
-            produces = "application/json",
-            response = HealthResponse.class
-    )
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "OK"),
-            @ApiResponse(code = 500, message = "Внутренняя ошибка")})
-    public HealthResponse getHealth() {
-        final HealthResponse healthResponse = new HealthResponse();
-        healthResponse.setStarted(true);
-        return healthResponse;
-    }
-
-    private class HealthResponse {
-        private boolean started;
-
-        public void setStarted(boolean started) {
-            this.started = started;
-        }
-
-        public boolean isStarted() {
-            return started;
-        }
+    @RequestMapping(value = "/health", method = RequestMethod.GET)
+    @ResponseBody
+    public String getHealth() {
+        return "ok";
     }
 }
